56f50936883fcaa89274c624eeee200706f2a018
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2021-12-29 15:22:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement("H1");
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = 
+    `<H1>Chris is the champion</H1>`
+
+
